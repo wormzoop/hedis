c481dd4dda3b23eb052dee7af838084278992f1e
wormzoop <446706489@qq.com>
2018-02-01 22:55:39 +0800

    q

---
+++ src/com/zoop/http/RamData.java
@@ -0,0 +1,18 @@
+package com.zoop.http;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 内存数据
+ */
+public class RamData {
+
+	//声明缓存对象,缓存数据就存在map中
+	public static Map<String, Object> map;
+	
+	static {
+		map = new HashMap<String, Object>();//实例化缓存对象
+	}
+	
+}
