0cb8a79be3fcf679e91efc8c583590602c1d710e
wormzoop <446706489@qq.com>
2018-02-02 15:15:04 +0800

    1

--- src/com/zoop/http/HttpServer.java
+++ src/com/zoop/http/HttpServer.java
@@ -1,10 +1,71 @@
 package com.zoop.http;
 
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.zoop.util.Config;
+
 /**
  * 处理网络请求
  */
 public class HttpServer {
 
+	public void accept() {
+		ServerSocket server = null;
+		ExecutorService executor = null;
+		try {
+			server = new ServerSocket(Config.port);
+			System.out.println("server socket is started");
+			executor = Executors.newCachedThreadPool();
+			while(true) {
+				Socket socket = server.accept();
+				executor.execute(new SocketHandler(socket));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(server != null) {
+					server.close();
+				}
+				if(executor != null){
+					executor.shutdown();
+				}
+			}catch(Exception e) {
+				
+			}
+		}
+	}
 	
+	class SocketHandler implements Runnable{
+
+		private Socket socket;
+		
+		public SocketHandler(Socket socket) {
+			this.socket = socket;
+		}
+		
+		@Override
+		public void run() {
+			InputStream in = null;
+			try {
+				socket.getInputStream();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				try {
+					if(in != null) {
+						in.close();
+					}
+				}catch(Exception e) {
+					
+				}
+			}
+		}
+		
+	}
 	
 }
--- src/com/zoop/http/RamData.java
+++ src/com/zoop/http/RamData.java
@@ -9,10 +9,6 @@ import java.util.Map;
 public class RamData {
 
 	//声明缓存对象,缓存数据就存在map中
-	public static Map<String, Object> map;
-	
-	static {
-		map = new HashMap<String, Object>();//实例化缓存对象
-	}
+	public static Map<String, Object> map = new HashMap<String, Object>();
 	
 }
